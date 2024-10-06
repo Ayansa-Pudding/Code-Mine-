fc24a527cf307e78b57ae5ecf4cc339165cab2e4
Ayansa-Pudding <ayansa.liyanage@outlook.com>
2024-10-05 22:25:46 -0400

    bit6.py

--- .vscode/04.10.2024/bit6.py
+++ .vscode/04.10.2024/bit6.py
@@ -1,6 +1,13 @@
-def fac(n):
-    if (n==1 or n==-1):
-        return 1
-    return n*fac(n-1)
-n=int(input("Enter your number:"))
-print(fac(n))
+# def fac(n):
+#     if (n==1 or n==-1):
+#         return 1
+#     return n*fac(n-1)
+# n=int(input("Enter your number:"))
+# print(fac(n))
+
+def Queu(n):
+    if n>10:
+        return
+    print(n)
+    Queu(n+1)
+Queu(2)
