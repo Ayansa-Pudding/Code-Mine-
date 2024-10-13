3e7e46305467082a694698ec00ff0c75d1578779
Ayansa-Pudding <ayansa.liyanage@outlook.com>
2024-10-12 22:28:25 -0400

    ror.py

---
+++ .vscode/04.10.2024/ror.py
@@ -0,0 +1,20 @@
+# arr=[1,4,9,16,25,36,49,64,81,100,121,144,169]
+# print(len(arr))
+
+
+# arr.append('98')
+# print(arr)
+
+# arr.sort()
+# print(arr)
+
+# arr.sort(reverse=True)
+# print(arr)
+
+arr=[1,4,9,16,25,36,49,64,81,100,121,144,169]
+# A= sum(arr)
+# print(A)
+big= max(arr)
+small= min(arr)
+print(big)
+print(small)
